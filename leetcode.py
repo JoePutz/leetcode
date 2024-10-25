092255a91d9255eb70e9b22921fc331d2e246016
Joe Putz <joseph.putz@smoothstack.com>
2024-10-25 16:00:49 -0500

    two sums

--- leetcode.py
+++ leetcode.py
@@ -42,3 +42,18 @@ class Solution(object):
             else:
                 total += rom_to_ara[s[i]]
         return total
+    
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}
+    
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
